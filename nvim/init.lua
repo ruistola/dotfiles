9269bf6a095a3d28888910537b9d05bfc7e93699
Juha Ruistola <juha.ruistola@metaplay.io>
2024-06-25 17:28:22 +0300

    Added neotest to neovim, adjusted tmux window status colors

--- nvim/init.lua
+++ nvim/init.lua
@@ -241,6 +241,26 @@ require("lazy").setup({
 		end,
 	},
 
+	{
+		'nvim-neotest/neotest',
+		dependencies = {
+			'thenbe/neotest-playwright',
+			dependencies = 'nvim-telescope/telescope.nvim',
+		},
+		config = function()
+			require('neotest').setup({
+				adapters = {
+				require('neotest-playwright').adapter({
+						options = {
+							persist_project_selection = true,
+							enable_dynamic_test_discovery = true,
+						},
+					}),
+				},
+			})
+		end,
+	}
+
 	-- JRu addition: Colorscheme (none of that onedark shit)
 
 	  -- {
