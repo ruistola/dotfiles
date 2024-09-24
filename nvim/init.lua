baeb4bddb614255ff11fb0c5171ad411b5311912
Juha Ruistola <juha.ruistola@metaplay.io>
2024-09-24 20:42:21 +0300

    Removed some crap, added LazyGit

--- nvim/init.lua
+++ nvim/init.lua
@@ -417,15 +417,25 @@ require("lazy").setup({
 		end,
 	},
 
-	-- JRu addition: Colorscheme (none of that onedark shit)
-
-	-- {
-	--   'savq/melange-nvim',
-	--   priority = 1000,
-	--   config = function()
-	--     vim.cmd.colorscheme 'melange'
-	--   end,
-	-- },
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		-- setting the keybinding for LazyGit with 'keys' is recommended in
+		-- order to load the plugin when the command is run for the first time
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+	},
 
 	-- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
 	--       These are some example plugins that I've included in the kickstart repository.
