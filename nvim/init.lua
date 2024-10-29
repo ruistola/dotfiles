11a73319d22511180c846cb1dc22556395f073e7
Juha Ruistola <juha.ruistola@metaplay.io>
2024-10-29 18:25:25 +0200

    Disabled blink.cmp autoselection

--- nvim/init.lua
+++ nvim/init.lua
@@ -195,6 +195,7 @@ require("lazy").setup({
 			highlight = { use_nvim_cmp_as_default = true },
 			nerd_font_variant = "normal",
 			trigger = { signature_help = { enabled = true } },
+			windows = { autocomplete = { selection = "manual" } },
 
 			keymap = {
 				show = "<C-space>",
