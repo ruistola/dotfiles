3b527f3a5590e14725262a3cebcf3f907b1db4c0
Juha Ruistola <juha.ruistola@metaplay.io>
2025-01-02 14:01:37 +0200

    Various fixes to blink.cmp

--- nvim/init.lua
+++ nvim/init.lua
@@ -197,54 +197,80 @@ require("lazy").setup({
 
 	{
 		"saghen/blink.compat",
-		-- use the latest release, via version = '*', if you also use the latest release for blink.cmp
 		version = "*",
-		-- lazy.nvim will automatically load the plugin when it's required by blink.cmp
 		lazy = true,
-		-- make sure to set opts so that lazy.nvim calls blink.compat's setup
 		opts = {
 			impersonate_nvim_cmp = true,
 		},
 	},
 
 	{
+		"hrsh7th/nvim-cmp",
+	},
+
+	{
+		"hrsh7th/cmp-cmdline",
+		dependencies = "hrsh7th/nvim-cmp",
+	},
+
+	{
+		"hrsh7th/cmp-path",
+		dependencies = "hrsh7th/nvim-cmp",
+	},
+
+	{
 		"saghen/blink.cmp",
-		-- optional: provides snippets for the snippet source
-		dependencies = "rafamadriz/friendly-snippets",
-
-		-- use a release tag to download pre-built binaries
+		lazy = false,
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-path",
+		},
 		version = "*",
-		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-		-- build = 'cargo build --release',
-		-- If you use nix, you can build from source using latest nightly rust with:
-		-- build = 'nix run .#build-plugin',
-
-		---@module 'blink.cmp'
-		---@type blink.cmp.Config
 		opts = {
-			-- 'default' for mappings similar to built-in completion
-			-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-			-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-			-- See the full "keymap" documentation for information on defining your own keymap.
-			keymap = { preset = "default" },
-
 			appearance = {
-				-- Sets the fallback highlight groups to nvim-cmp's highlight groups
-				-- Useful for when your theme doesn't support blink.cmp
-				-- Will be removed in a future release
 				use_nvim_cmp_as_default = true,
-				-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-				-- Adjusts spacing to ensure icons are aligned
 				nerd_font_variant = "mono",
 			},
-
-			-- Default list of enabled providers defined so that you can extend it
-			-- elsewhere in your config, without redefining it, due to `opts_extend`
-			sources = {
-				default = { "lsp", "path", "snippets", "buffer" },
+			completion = {
+				list = {
+					selection = "manual",
+				},
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 500,
+				},
+			},
+			signature = { enabled = true },
+			keymap = {
+				preset = "default",
+				["<Tab>"] = { "select_next" },
+				["<S-Tab>"] = { "select_prev" },
+				["<CR>"] = { "accept", "fallback" },
 			},
 		},
-		opts_extend = { "sources.default" },
+
+		config = function(_, opts)
+			local blink_cmp = require("blink.cmp")
+			blink_cmp.setup(opts)
+
+			local cmp = require("cmp")
+
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "path" }, -- optional path completion
+					{ name = "cmdline", keyword_length = 2 }, -- main cmdline source
+				},
+			})
+
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+		end,
 	},
 
 	{
