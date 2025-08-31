3cd768347af4a3aae1786aed0e80b7cafdf45099
Juha Ruistola <juha.ruistola@metaplay.io>
2025-08-31 10:56:28 +0300

    Debugger config, mostly keymaps. Removed C#/Unity stuff

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -9,11 +9,172 @@ return {
 				dependencies = { "nvim-neotest/nvim-nio" },
 			},
 		},
+		keys = {
+			{
+				"<leader>dc",
+				function()
+					require("dap").continue()
+				end,
+				desc = "Debug: Continue",
+			},
+			{
+				"<leader>dso",
+				function()
+					require("dap").step_over()
+				end,
+				desc = "Debug: Step Over",
+			},
+			{
+				"<leader>dsi",
+				function()
+					require("dap").step_into()
+				end,
+				desc = "Debug: Step Into",
+			},
+			{
+				"<leader>dst",
+				function()
+					require("dap").step_out()
+				end,
+				desc = "Debug: Step Out",
+			},
+			{
+				"<leader>db",
+				function()
+					require("dap").toggle_breakpoint()
+				end,
+				desc = "Debug: Toggle Breakpoint",
+			},
+			{
+				"<leader>dB",
+				function()
+					require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+				end,
+				desc = "Debug: Set Conditional Breakpoint",
+			},
+			{
+				"<leader>dlp",
+				function()
+					require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+				end,
+				desc = "Debug: Set Log Point",
+			},
+			{
+				"<leader>dr",
+				function()
+					require("dap").repl.open()
+				end,
+				desc = "Debug: Open REPL",
+			},
+			{
+				"<leader>dl",
+				function()
+					require("dap").run_last()
+				end,
+				desc = "Debug: Run Last",
+			},
+			{
+				"<leader>dh",
+				function()
+					require("dap.ui.widgets").hover()
+				end,
+				desc = "Debug: Hover Variables",
+				mode = { "n", "v" },
+			},
+			{
+				"<leader>dp",
+				function()
+					require("dap.ui.widgets").preview()
+				end,
+				desc = "Debug: Preview",
+				mode = { "n", "v" },
+			},
+			{
+				"<leader>df",
+				function()
+					local widgets = require("dap.ui.widgets")
+					widgets.centered_float(widgets.frames)
+				end,
+				desc = "Debug: Show Frames",
+			},
+			{
+				"<leader>ds",
+				function()
+					local widgets = require("dap.ui.widgets")
+					widgets.centered_float(widgets.scopes)
+				end,
+				desc = "Debug: Show Scopes",
+			},
+
+			-- DAP-UI specific keymaps
+			{
+				"<leader>du",
+				function()
+					require("dapui").toggle()
+				end,
+				desc = "Debug: Toggle UI",
+			},
+			{
+				"<leader>de",
+				function()
+					require("dapui").eval()
+				end,
+				desc = "Debug: Eval",
+				mode = { "n", "v" },
+			},
+
+			-- Go specific keymaps
+			{
+				"<leader>dgt",
+				function()
+					require("dap-go").debug_test()
+				end,
+				desc = "Debug: Go Test",
+			},
+			{
+				"<leader>dgl",
+				function()
+					require("dap-go").debug_last_test()
+				end,
+				desc = "Debug: Go Last Test",
+			},
+		},
 		config = function()
 			local dap, dapui = require("dap"), require("dapui")
 
 			-- DAP UI setup
-			dapui.setup()
+			dapui.setup({
+				icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
+				mappings = {
+					-- Use a table to apply multiple mappings
+					expand = { "<CR>", "<2-LeftMouse>" },
+					open = "o",
+					remove = "d",
+					edit = "e",
+					repl = "r",
+					toggle = "t",
+				},
+				layouts = {
+					{
+						elements = {
+							{ id = "scopes", size = 0.25 },
+							{ id = "breakpoints", size = 0.25 },
+							{ id = "stacks", size = 0.25 },
+							{ id = "watches", size = 0.25 },
+						},
+						size = 40,
+						position = "left",
+					},
+					{
+						elements = {
+							{ id = "repl", size = 0.5 },
+							{ id = "console", size = 0.5 },
+						},
+						size = 10,
+						position = "bottom",
+					},
+				},
+			})
 
 			-- Auto open/close DAP UI
 			dap.listeners.before.attach.dapui_config = function()
@@ -28,6 +189,28 @@ return {
 			dap.listeners.before.event_exited.dapui_config = function()
 				dapui.close()
 			end
+
+			-- Visual indicators for breakpoints
+			vim.fn.sign_define(
+				"DapBreakpoint",
+				{ text = "🛑", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+			)
+			vim.fn.sign_define(
+				"DapBreakpointCondition",
+				{ text = "🟡", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+			)
+			vim.fn.sign_define(
+				"DapBreakpointRejected",
+				{ text = "🚫", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+			)
+			vim.fn.sign_define(
+				"DapLogPoint",
+				{ text = "📝", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
+			)
+			vim.fn.sign_define(
+				"DapStopped",
+				{ text = "➡️", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" }
+			)
 
 			-- Go debugging
 			require("dap-go").setup({
@@ -52,37 +235,6 @@ return {
 					verbose = false,
 				},
 			})
-
-			-- Unity/C# debugging
-			dap.adapters.vstuc = {
-				type = "executable",
-				command = "/opt/homebrew/bin/mono",
-				args = {
-					"/Users/ruistola/.vscode/extensions/visualstudiotoolsforunity.vstuc-1.0.2/bin/UnityDebugAdapter.dll",
-				},
-			}
-
-			dap.adapters.coreclr = {
-				type = "executable",
-				command = "/usr/local/bin/netcoredbg/netcoredbg",
-				args = { "--interpreter=vscode" },
-			}
-
-			dap.configurations.cs = {
-				{
-					type = "coreclr",
-					name = "launch - netcoredbg",
-					request = "launch",
-					program = function()
-						return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
-					end,
-				},
-				{
-					type = "vstuc",
-					request = "attach",
-					name = "Unity Editor",
-				},
-			}
 		end,
 	},
 }
